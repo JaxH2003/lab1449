2bb90680c7171fb96653550137ea4c1d4c31e5b3
chickennugget <hefkejac@msu.edu>
2024-01-22 15:39:57 -0500

    test 3

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,4 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("styles");
     eleventyConfig.addPassthroughCopy("src");
-  };
-  module.exports = function (eleventyConfig) {
-    return {
-        dir: {
-            input: "src"
-            output: "_site"
-        },
-    };
-  };
+}
